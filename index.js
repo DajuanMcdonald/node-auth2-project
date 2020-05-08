58524b62f9c414a0d3ccccf585f94bf767c36809
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-05-08 11:38:14 -0400

    update dotenv script

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
+
 const app = express();
 app.use(express.json());
+
+const users = [
+    {
+        username: 'Juan',
+        deparment: 'IT'
+    },
+
+    {
+        username: 'Sherr',
+        department: 'Administration'
+    },
+
+    {
+        username: 'Daniel',
+        department: 'Accounting'
+    }
+
+
+]
 
 app.post('/api/register', (req, res) => {
 
@@ -19,9 +39,24 @@ app.post('/api/login', (req, res) => {
 
 })
 
-app.get('/api/users', (req, res) => {
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
 
 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403)
+
+        req.user = user
+        next()
+    })
+
+
+}
+
+app.get('/api/users', authenticateToken, (req, res) => {
+    res.json(users.filter(user => user.username === req.user.name))
 })
 
 const PORT = process.env.PORT || 5002;
